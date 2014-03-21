9f67d214bd1e8b57dbd955a0340b40599f36e6fb
Alan Guevara <alangvara@gmail.com>
2014-03-21 13:24:47 -0600

    se implementa composer y algunos arreglos a la clase PHPUnit

--- PHPUtil.php
+++ PHPUtil.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace Alangvara\AssetPipeline;
+namespace Alangvara\PHPUtil;
 
 class PHPUtil{
 	
-	public static function getRandomString($length, $numeric = true, $uppercase = false,
+	public static function generateRandomString($length, $numeric = true, $uppercase = false,
 		$specialChars = false){
 		
 		$source = 'abcdefghijklmnopqrstuvwxyz';
 		
-    if($uppercase){
-    	$source .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		}
-		
     if($numeric){
     	$source .= '1234567890';
+		}
+		
+    if($uppercase){
+    	$source .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 		}
 		
     if($specialChars==1){
@@ -25,7 +25,7 @@ class PHPUtil{
       $rstr = "";
       $source = str_split($source,1);
       for($i=1; $i<=$length; $i++){
-          $num = rand(0, count($source));
+          $num = rand(0, count($source) - 1);
           $rstr .= $source[$num];
       }
     }
